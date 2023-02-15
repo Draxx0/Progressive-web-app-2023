eb4efcf75aa63b6e0f476bd8006e3179eca779a5
Draxx0 <williamfort.lmgl@gmail.com>
2023-02-15 15:36:10 +0100

    ??

--- src/app/views/MenuScreen.tsx
+++ src/app/views/MenuScreen.tsx
@@ -5,40 +5,43 @@ import { Game } from "../../api/db/utils";
 import { GameContext } from "../contexts/gameContext";
 
 const MenuScreen = () => {
-  const {game, setGame} = useContext(GameContext)
+  const { game, setGame } = useContext(GameContext);
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    console.log(game);
-    
-    
-  }, []);
-  console.log("game :", game);
 
-  const handleSubmit = () => {    
-    
-  };
+  const handleSubmit = () => {};
 
   return (
-    <div 
-    className="menu-screen" 
-    style={{ backgroundImage: `url('./assets/images/remote-menu-bg.jpg')`}}
+    <div
+      className="menu-screen"
+      style={{ backgroundImage: `url('./assets/images/remote-menu-bg.jpg')` }}
     >
-    
-        <div className="userName">
-        <img src="./assets/images/userName-input.png" alt="userName" className="userName__image"/>
+      <div className="userName">
+        <img
+          src="./assets/images/userName-input.png"
+          alt="userName"
+          className="userName__image"
+        />
         <input
           type="text"
           placeholder="Enter username"
           // onChange={(e) => setUsername(e.target.value)}
           className="userName__input"
         />
-        </div>
+      </div>
       <div className="roche">
-        <img src="./assets/images/button-ok.png" alt="roche" className="roche__image" onClick={handleSubmit}/>
+        <img
+          src="./assets/images/button-ok.png"
+          alt="roche"
+          className="roche__image"
+          onClick={handleSubmit}
+        />
       </div>
       <div className="feuille">
-        <img src="./assets/images/feuilleu.png" alt="feuille" className="feuille__image"/>
+        <img
+          src="./assets/images/feuilleu.png"
+          alt="feuille"
+          className="feuille__image"
+        />
       </div>
     </div>
   );
